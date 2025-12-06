74d9ffead757f54848cc972acd5bc01985fdcbbe
DanielleFiene <daniellefiene@hotmail.com>
2025-12-06 16:11:35 +0100

    initial commit

---
+++ resources/JS/main.js
@@ -0,0 +1,7 @@
+// Dynamisch jaar updaten in footer
+document.addEventListener('DOMContentLoaded', function() {
+  const yearElement = document.getElementById('current-year');
+  if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+  }
+});
